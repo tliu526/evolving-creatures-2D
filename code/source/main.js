66a60e785d25ce346d08f0af3bfa45d3adc69493
tliu526 <tl4@williams.edu>
2015-11-19 01:29:07 -0500

    Updated journal, moved creature init to generateRandomCreature() in util.js

--- code/source/main.js
+++ code/source/main.js
@@ -5,79 +5,8 @@ function onLoad() {
     canvas = document.getElementById("c");
     var ctx = canvas.getContext("2d");
 
-    //add our masses to the world
-    for(var i = 0; i < 10; i++){
-        var mass_options = {
-            x           : Math.random()*(canvas.width-50) + 50,
-            y           : ((Math.random()*canvas.height) / 2),
-            density     : 1.0,
-            restitution : 0.2,
-            friction    : 0.5,
-            isStatic    : false
-        }
-        //console.log("x: " + mass_options.x);
-        //console.log("y: " + mass_options.y);
-        var mass = new Mass(mass_options);
-        components.push(mass);
-        masses.push(mass);
-        //components[i].addToWorld();
-    }
-
-    var connected = new Array(masses.length * masses.length);
-    for(var i = 0; i < connected.length; i++) {
-	connected[i] = false;
-    }
-
-    for(var i = 0; i < 20; i++) {
-	
-       iA = Math.floor(Math.random()*masses.length);
-       iB = Math.floor(Math.random()*masses.length);
-       while (iA == iB 
-        || connected[iA + masses.length * iB] 
-        || connected[iB + masses.length * iA]) {
-        iB = Math.floor(Math.random()*masses.length);
-    }
-
-	connected[iA + masses.length * iB] = true;
-	connected[iB + masses.length * iA] = true;
-
-        var mA = masses[iB]
-        var mB = masses[iA]
-
-	
-	if (Math.random() < 0.5) {
-	    var spring_options = {
-		massA : mA,             
-		massB : mB,
-		restLength : Math.random()*50 + 15,
-		damping : Math.random() / 2.0,
-		frequency : Math.random()*50
-	    }
-	    
-	    var spring = new Spring(spring_options);
-        connections.push(spring);
-	    //spring.addToWorld();
-	} else {
-	    theta = Math.random()*Math.PI*2;
-
-	    var muscle_options = {
-          massA : mA,             
-          massB : mB,
-          lowerLimit : 0,
-          upperLimit : Math.random()*1.5 + 1.0,
-          motorSpeed : Math.random()*4.0 + 1.0,
-          maxMotorForce: Math.random()*300.0 + 200.0,
-          axis : new b2Vec2(Math.cos(theta), Math.sin(theta))
-      }
-
-	    var muscle = new Muscle(muscle_options);
-        connections.push(muscle);
-	    //muscle.addToWorld();
-	}
-    }
-
     //instantiate the creature
-    var creature = new Creature(masses, connections);
+    var creature = generateRandomCreature();
     creature.addToWorld();
 
     //add boundaries
